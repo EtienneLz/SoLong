74e238077cdfa29693f5bc7ffe2becabb7c10eac
elouchez <elouchez@stud.42.fr>
2021-06-13 22:05:18 +0200

    Third push

--- includes/solong.h
+++ includes/solong.h
@@ -16,45 +16,52 @@
 # define LEFT		    97
 # define BUFFER_SIZE    4096
 
-typedef struct  s_data 
+typedef struct      s_data 
 {
-    void        *img;
-    char        *addr;
-    int         bits_per_pixel;
-    int         line_length;
-    int         endian;
-    int         res_x;
-    int         res_y;
-}               t_data;
+    void            *img;
+    char            *addr;
+    int             bits_per_pixel;
+    int             line_length;
+    int             endian;
+    int             res_x;
+    int             res_y;
+}                   t_data;
 
-typedef struct  s_map_data
+typedef struct      s_map_data
 {
-    char        **map;
-    int         size_map;
-    int         size_line_max;
-    t_data      name;
-}               t_map_data;
+    char            **map;
+    int             size_map;
+    int             size_line_max;
+}                   t_map_data;
 
-typedef struct  s_var_mlx 
+typedef struct      s_var_mlx 
 {
-    void        *mlx;
-    void        *win;
-    int         size_map;
-    size_t      size_line_max;
-    int         size_case;
-}               t_var_mlx;
+    void            *mlx;
+    void            *win;
+    int             size_map;
+    size_t          size_line_max;
+    int             size_case;
+}                   t_var_mlx;
 
-typedef struct  s_g_struct
+typedef struct      s_check_flags
 {
-    t_data      data;
-    t_map_data  map_data;
-    t_var_mlx   var_mlx;
-}               t_struct;
+    int             pos_i;
+    int             pos_j;
+    int             s_pos_i;
+    int             s_pos_j;
+    int             init_done;
+}                   t_check_flags;
 
 
+typedef struct      s_struct
+{
+    t_data          data;
+    t_map_data      map_data;
+    t_var_mlx       var_mlx;
+    t_check_flags   check_flags;
+}                   t_struct;
 
-void    ft_file_read(char *file_name, t_struct *stru);
-
+void            ft_file_read(char *file_name, t_struct *stru);
 int		        get_next_line(int fd, char **line);
 size_t	        ft_strlen(const char *s);
 char	        *ft_strjoin(char const *s1, char const *s2);
@@ -63,5 +70,8 @@ char	        *ft_strchr(const char *s, int c);
 char	        *ft_substr(char const *s, unsigned int start, size_t len);
 char	        *clean_line(char *save, char **line);
 char	        *ft_save(char *buffer, char *save);
+void            draw_player(double d_i, double d_j, t_struct *stru);
+void            win_init(t_struct *stru);
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color);
 
 #endif
--- srcs/win_init.c
+++ srcs/win_init.c
@@ -1,81 +1,81 @@
-#include "../includes/cube.h"
+#include "../includes/solong.h"
 
-void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void            my_mlx_pixel_put(s_data *data, int x, int y, int color)
 {
     char    *dst;
 
-    dst = struct->data->addr + (y * struct->data->line_length + x * (struct->data->bits_per_pixel / 8));
+    dst = stru->data->addr + (y * stru->data->line_length + x * (stru->data->bits_per_pixel / 8));
     *(unsigned int*)dst = color;
 }
 
-/*static void     draw_square(int i, int j)
+static void     draw_square(int i, int j, t_struct *stru)
 {
     int     p;
     int     q;
 
     p = 0;
-    while (p <= g_vars.size_case)
+    while (p <= stru->var_mlx.size_case)
     {
         q = 0;
-        while (q <= g_vars.size_case)
+        while (q <= stru->var_mlx.size_case)
         {
-            if (g_data.map[i][j] == '1')
-                my_mlx_pixel_put(&g_data, g_vars.size_case * j + q, g_vars.size_case * i + p, 0x00999999);
-            else if (g_data.map[i][j] == '0')
-                my_mlx_pixel_put(&g_data, g_vars.size_case * j + q, g_vars.size_case * i + p, 0x00FFFFFF);
-            else if (g_data.map[i][j] == ' ')
-                my_mlx_pixel_put(&g_data, g_vars.size_case * j + q, g_vars.size_case * i + p, 0x00000000);
+            if (stru->map_data.map[i][j] == '1')
+                my_mlx_pixel_put(&stru, stru->var_mlx.size_case * j + q, stru->var_mlx.size_case * i + p, 0x00999999);
+            else if (stru->map_data.map[i][j] == '0')
+                my_mlx_pixel_put(&stru, stru->var_mlx.size_case * j + q, stru->var_mlx.size_case * i + p, 0x00FFFFFF);
+            else if (stru->map_data.map[i][j] == ' ')
+                my_mlx_pixel_put(&stru, stru->var_mlx.size_case * j + q, stru->var_mlx.size_case * i + p, 0x00000000);
             else
-                my_mlx_pixel_put(&g_data, g_vars.size_case * j + q, g_vars.size_case * i + p, 0x00FFFFFF);
+                my_mlx_pixel_put(&stru, stru->var_mlx.size_case * j + q, stru->var_mlx.size_case * i + p, 0x00FFFFFF);
             q++;
         }
         p++;
     }
-}*/
+}
 
-void    draw_player(double d_i, double d_j)
+void    draw_player(double d_i, double d_j, t_struct *stru)
 {
     //int p;
     //int q;
     int j;
     int i;
 
-    if (g_check_flags.init_done == 0)
+    if (stru->check_flags.init_done == 0)
     {
-        j = (struct->vars.size_case * struct->check_flags.s_pos_j + struct->var.size_case / 2 - 4);
-        i = (struct->vars.size_case * struct->check_flags.s_pos_i + var.size_case / 2 - 4);
-        struct->check_flags.pos_i = i + 4;
-        struct->check_flags.pos_j = j + 4;
+        j = (stru->var_mlx.size_case * stru->check_flags.s_pos_j + stru->var_mlx.size_case / 2 - 4);
+        i = (stru->var_mlx.size_case * stru->check_flags.s_pos_i + stru->var_mlx.size_case / 2 - 4);
+        stru->check_flags.pos_i = i + 4;
+        stru->check_flags.pos_j = j + 4;
     }
     else
     {
         j = d_j - 4;
         i = d_i - 4;
-        struct->g_check_flags.pos_i = d_i;
-        struct->g_check_flags.pos_j = d_j;
+        stru->check_flags.pos_i = d_i;
+        stru->check_flags.pos_j = d_j;
     }
     printf("%f, %f bloup\n", d_i, d_j);
-    /*p = 0;
+    p = 0;
     while (p <= 9)
     {
         q = 0;
         while (q <= 9)
         {
-            my_mlx_pixel_put(&g_data, j + q, i + p, 0x00FF0000);
+            my_mlx_pixel_put(&stru->data, j + q, i + p, 0x00FF0000);
             q++;
         }
         p++;
-    }*/
+    }
 }
 
-void    win_init(void)
+void    win_init(t_struct *stru)
 {
     /*int i;
     int j;
 
     i = 0;*/ 
-    struct->data.img = mlx_new_image(struct->var.mlx, g_data.res_x, g_data.res_y);
-    struct->data.addr = mlx_get_data_addr(struct->data.img, &g_data.bits_per_pixel, &g_data.line_length, &g_data.endian);
+    stru->data.img = mlx_new_image(stru->var_mlx.mlx, stru->data.res_x, stru->data.res_y);
+    stru->data.addr = mlx_get_data_addr(stru->data.img, &stru->data.bits_per_pixel, &stru->data.line_length, &stru->data.endian);
     /*while (g_data.map[i])
     {
         j = 0;
@@ -86,10 +86,10 @@ void    win_init(void)
         }
         i++;
     }*/
-    if (struct->check_flags.init_done == 0)
+    if (stru->check_flags.init_done == 0)
     {
-        struct->check_flags.pos_i = struct->var.size_case * struct->check_flags.s_pos_i + struct->var.size_case / 2 - 4;
-        struct->check_flags.pos_j = struct->var.size_case * struct->check_flags.s_pos_j + struct->var.size_case / 2 - 4;
+        stru->check_flags.pos_i = stru->var_mlx.size_case * stru->check_flags.s_pos_i + stru->var_mlx.size_case / 2 - 4;
+        stru->check_flags.pos_j = stru->var_mlx.size_case * stru->check_flags.s_pos_j + stru->var_mlx.size_case / 2 - 4;
         //draw_player(g_check_flags.pos_i, g_check_flags.pos_j);
     }
 }
